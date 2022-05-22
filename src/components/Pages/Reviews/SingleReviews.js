07b573e130b01a5c1da5b139772d07fbf4f3650e
robinrakibul <robinrakibulhassan@gmail.com>
2022-05-22 17:22:01 +0600

    fixed console.log errors

--- src/components/Pages/Reviews/SingleReviews.js
+++ src/components/Pages/Reviews/SingleReviews.js
@@ -4,13 +4,14 @@ const SingleReviews = ({ review }) => {
     const { name, rating, comment, image } = review;
     return (
             <div className="card w-96 bg-base-100 border-2 shadow-xl">
-                <figure><img src={review.image} className="rounded-full mt-5" alt="" /></figure>
+                <figure><img src={image} className="rounded-full mt-5" alt="" /></figure>
                 <div className="card-body">
+                    <p>{name}</p>
                     <h2 className="card-title">
-                    <p>Ratings: {review.rating} <input type="radio" name="rating-4" disabled className="mask mask-star-2 bg-green-500" /></p>
+                    <p>Ratings: {rating} <input type="radio" name="rating-4" disabled className="mask mask-star-2 bg-green-500" /></p>
                     <div className="badge rounded-lg badge-accent">Buyer Comment</div>
                     </h2>
-                    <p>{review.comment}</p>
+                    <p>{comment}</p>
                     <div className="card-actions justify-end">
                         <div className="badge badge-outline">Car Parts</div>
                     </div>
