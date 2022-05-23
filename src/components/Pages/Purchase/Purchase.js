fe6281a9d9634601e50035849cbcfa22d3429d27
robinrakibul <robinrakibulhassan@gmail.com>
2022-05-23 17:59:46 +0600

    added conditions to handle purchasing, purchase page completed

--- src/components/Pages/Purchase/Purchase.js
+++ src/components/Pages/Purchase/Purchase.js
@@ -1,28 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
-import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 
 const Purchase = () => {
 
     // For quantity
     const [items, setitems] = useState([]);
+    const quantityInputRef = useRef('');
 
     useEffect(() => {
         const url = `https://manufacturer-node-server.herokuapp.com/items/${itemsId}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setitems(data))
-    }, [items]) /* updates everytime item quantity changes */
+    }, [items])
 
-    const { register, handleSubmit } = useForm();
-
-    const onSubmit = async data => {
-        // data.address use this to store address
-    }
     const { itemsId } = useParams();
     const [user] = useAuthState(auth);
+
+    const handleOrder = event => {
+        event.preventDefault();
+
+        const order = {
+            orderId: items._id,
+            orderItemName: items.title,
+            orderItemPrice: items.price,
+            orderQuantity: event.target.quantityInput.value,
+            orderAddress: event.target.address.value,
+        }
+
+        fetch('http://localhost:5000/order', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.acknowledged==true){
+                    toast.success('Order successfully added')
+                }
+                else(
+                    toast.error('Order failed, try again later')
+                )
+            })
+    }
     return (
         <div className='flex justify-center'>
             <div className="max-w-lg bg-white text-red rounded-lg shadow-md border-2 pt-5 pl-5 pr-5 m-5">
@@ -33,20 +59,23 @@ const Purchase = () => {
                 <h5 className="text-lg tracking-tight text-gray-900 dark:text-white"><span className='font-semibold'>Product Name: </span>{items.title}</h5>
                 <img className="p-4 rounded-lg border-2 mt-2 mb-2 w-96" src={items.image} alt="" />
                 <h5 className="text-lg tracking-tight text-gray-900 dark:text-white"><span className='font-semibold'>Description: </span>{items.description}</h5>
-                <h5 className="text-xl border-t-2 border-b-2 border-yellow-400 tracking-tight text-gray-900 dark:text-white">Available Total: <span className='font-bold'>{items.quantity}</span></h5>
-                <h5 className="text-xl border-yellow-400 tracking-tight text-gray-900 dark:text-white">Minimum Order Quantity: <span className='font-bold'>{items.minOrder}</span></h5>
+                <h5 className="text-xl border-t-2 border-b-2 border-yellow-400 tracking-tight text-gray-900 dark:text-white">Available Quantity: <span className='font-bold'>{items.quantity}</span></h5>
                 <hr />
-                <form onSubmit={handleSubmit(onSubmit)} className='grid grid-rows-2 gap-4 mt-10'>
-                    <textarea
-                        type="text"
-                        placeholder="Your Address"
-                        className="input input-bordered w-full"
-                        {...register("address", {
-                            required: {
-                                value: true,
-                                message: 'Address is Required'
-                            }
-                        })}
+                <form className='grid grid-rows-2 gap-4 mt-10' onSubmit={handleOrder}>
+                    <h5 className="text-xl border-yellow-400 tracking-tight text-gray-900 dark:text-white">Insert Your Order Quantity:
+                        <span className='font-bold'>
+                            <input min={items.minOrder} max={items.quantity}
+                                type="number" name='quantityInput' required
+                                placeholder="Order Quantity" ref={quantityInputRef}
+                                className="rounded-lg input-bordered w-full"
+                            />
+                        </span>
+                    </h5>
+                    <textarea className='rounded-lg input-bordered w-full' name="address" placeholder="Your Address" type="text" required></textarea>
+                    <input
+                        type="number" name='number' required
+                        placeholder="Your Phone Number"
+                        className="rounded-lg input-bordered w-full"
                     />
                     <div className='grid justify-center text-white'>
                         <button className='pt-3 pb-3 pl-4 pr-4 w-52 rounded bg-yellow-400 hover:bg-yellow-500 mb-5' data-mdb-ripple="true" data-mdb-ripple-color="light">Complete Purchase</button>
