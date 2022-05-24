baee3a2291b8f8939d9633bc0f24768fcca5620d
robinrakibul <robinrakibulhassan@gmail.com>
2022-05-24 12:54:33 +0600

    Only admin can make admin, users restricted to make admin

--- src/components/Pages/Dashboard/AllUsersData.js
+++ src/components/Pages/Dashboard/AllUsersData.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { toast } from 'react-toastify';
 
 const AllUsersData = ({ user, refetch }) => {
-    const { email,role } = user;
+    const { email, role } = user;
     const makeAdmin = () => {
         fetch(`http://localhost:5000/user/admin/${email}`, {
             method: 'PUT',
@@ -10,10 +10,18 @@ const AllUsersData = ({ user, refetch }) => {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
             }
         })
-            .then(res => res.json())
+            .then(res => {
+                if (res.status === 403) {
+                    toast.error('Failed to Make an admin');
+                }
+                return res.json()
+            })
             .then(data => {
-                refetch();
-                toast.success(`Successfully made an admin`);
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    toast.success(`Successfully made an admin`);
+                }
+
             })
 
     }
